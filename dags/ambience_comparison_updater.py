352a330ffb9fdfecd2641af06658e78ece9902e9
John Snyder <snyderjo@gmail.com>
2025-03-18 19:14:29 -0600

    changes val to value, and macros.ds to ds--working now.

--- dags/ambience_comparison_updater.py
+++ dags/ambience_comparison_updater.py
@@ -21,17 +21,17 @@ with DAG(
     
     report_render = BashOperator(
         task_id = 'index_rmd'
-        ,bash_command = "cd {{ var.val.r_dir_ambComp }}; Rscript renderer.R"
+        ,bash_command = "cd {{ var.value.r_dir_ambComp }}; Rscript renderer.R"
     )
 
     mv_results = BashOperator(
         task_id='mv_to_github'
-        ,bash_command = "cd {{ var.val.r_dir_ambComp }}; mv -f index.md dailyReport.html {{ var.val.githubIO_dir }}ambience_comparison/; mv -f images/*.png {{ var.val.githubIO_dir }}ambience_comparison/images/"
+        ,bash_command = "cd {{ var.value.r_dir_ambComp }}; mv -f index.md dailyReport.html {{ var.value.githubIO_dir }}ambience_comparison/; mv -f images/*.png {{ var.value.githubIO_dir }}ambience_comparison/images/"
     )
 
     git_push = BashOperator(
         task_id = "update_githubIO"
-        ,bash_command = "cd {{ var.val.githubIO_dir }}ambience_comparison/; git add index.md dailyReport.html images/*.png; git commit -m 'updates results on {{ macros.ds }}'; git push origin master"
+        ,bash_command = "cd {{ var.value.githubIO_dir }}ambience_comparison/; git add index.md dailyReport.html images/*.png; git commit -m 'updates results on {{ ds }}'; git push origin master"
     )
 
     report_render >> mv_results >> git_push 
